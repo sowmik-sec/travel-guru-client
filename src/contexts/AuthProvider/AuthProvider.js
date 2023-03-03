b3b8f743d45d18ddfff6178348933467b4cbdc4b
sowmik-sec <sowmik.sec@gmail.com>
2023-03-03 22:40:53 +0600

    loader is important for private routing. loader added

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -13,26 +13,40 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
   const googleSignIn = (provider) => {
+    setLoading(true);
     return signInWithPopup(auth, provider);
   };
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const logIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
     return () => unSubscribe;
   }, []);
-  const authInfo = { user, setUser, googleSignIn, createUser, logIn, logOut };
+  const authInfo = {
+    user,
+    loading,
+    setUser,
+    googleSignIn,
+    createUser,
+    logIn,
+    logOut,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
--- src/privateRoute/PrivateRoute.js
+++ src/privateRoute/PrivateRoute.js
@@ -3,8 +3,11 @@ import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthProvider/AuthProvider";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+  if (loading) {
+    return <div>Loading.....</div>;
+  }
   if (!user) {
     return <Navigate to="/signin" state={{ from: location }} replace />;
   }
